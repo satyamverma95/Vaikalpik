8b46fb9ee27aa407a8c28eca2ecefb7c8225f497
satyamverma95 <satyamverma95@gmail.com>
2023-05-12 13:12:23 +0530

    Submitting Latest Submission
    Adding Code for Front End and Required Library

--- Source_code/Flask Application/Static/data_extractor.js
+++ Source_code/Flask Application/Static/data_extractor.js
@@ -20,205 +20,47 @@ function readTextFile(file)
 
 function populate_list(data_object){
 
-  //console.log("Data Received", cuisine_recipe_list)
-  cuisine_recipe_list = data_object //Stroing varibale in global varibale for access across all function.
-
-  var select = document.getElementById("cuisine_id");
-  var options = Object.keys(data_object)
-
-  el = document.createElement("option");
-  el.textContent = "None";
-  el.value = opt;
-  select.appendChild(el)
-
-  for(var i = 0; i < options.length; i++) {
-      var opt = options[i];
-      var el = document.createElement("option");
-      el.textContent = opt;
-      el.value = opt;
-      select.appendChild(el);
-  }
-
-}
-
-function populate_recipe(){
-
-  cuisine_selected = document.getElementById("cuisine_id").value
-  document.getElementById('recipe_id').innerHTML = ""
-
-  var select = document.getElementById("recipe_id");
-  var options = Object.keys(cuisine_recipe_list[cuisine_selected])
-
-
-  for(var i = 0; i < options.length; i++) {
-      var opt = options[i];
-      var el = document.createElement("option");
-      el.textContent = opt;
-      el.value = opt;
-      select.appendChild(el);
-  }
-
-
+  var treeJson = convertPythonJSONToTreeBasedJSON(data_object);
+  console.log(treeJson);
+  initializeComboTree(treeJson)
 }
 
 
-function fetch_recipe_details (){
+function convertPythonJSONToTreeBasedJSON(jsonObj) {
+  let idCounter = 1;
 
-  cuisine_selected = document.getElementById("cuisine_id").value
-  recipe_selected = document.getElementById("recipe_id").value
+  function processSubTopics(subTopics) {
+    const nodes = [];
 
-  document.getElementById('recipe_directions').innerHTML = "";
-  document.getElementById('Question_input_id').value = "";
-  document.getElementById('Answer_div').innerHTML = "";
-  direction_index = 1;
+    for (const key in subTopics) {
+      const subTopic = subTopics[key];
+      const id = idCounter++;
+      const node = { id, title: subTopic.Title };
 
-  console.log(cuisine_selected, recipe_selected )
-
-  $.ajax({
-    type: "POST",
-    url: "/create_file",
-    data: {
-      "cuisine" : cuisine_selected, 
-      "recipe"  : recipe_selected
-    },success: function (data, status, xhr) {
-      //console.log(data);
-      recipe_data = data
-      process_recipes_data('')
-    },
-    error: function (jqXhr, textStatus, errorMessage) {
-      console.log(errorMessage);
-    }
-  })
-
-}
-
-function process_recipes_data(call){
-
-  console.log(recipe_data)
-
-  document.getElementById('recipe_directions').innerHTML = ""
-  document.getElementById('Servings_id').value = ""
-
-
-  if (call.match("Next")){
-    direction_index += 1
-  }
-  else if (call.match("Prev")){
-    direction_index -= 1
-  }
-
-  if (direction_index > 1){
-    document.getElementById("Prev").disabled = false;
-  }
-  else{
-    document.getElementById("Prev").disabled = true;
-  }
-
-
-  if ( direction_index <= Object.keys(recipe_data.Directions).length){
-  
-      var para = document.createElement("p");
-      var node_1 = document.createTextNode("Step " + direction_index + ":");
-      para.appendChild(node_1);
-      var node_2 = document.createTextNode(recipe_data["Directions"][direction_index]);
-      para.appendChild(node_2);
-      var element = document.getElementById("recipe_directions");
-      element.appendChild(para);
-      document.getElementById("Next").disabled = false;
-
-      if (direction_index == Object.keys(recipe_data.Directions).length){
-        document.getElementById("Next").disabled = true;
+      if (subTopic["Sub Topics"]) {
+        node.subs = processSubTopics(subTopic["Sub Topics"]);
       }
-    
+
+      nodes.push(node);
     }
 
-    document.getElementById('Servings_id').value = recipe_data["Meta_Info"]["Servings:"]
-
-}
-
-function ask_question(speechToText){
-
-  
-  question_asked = document.getElementById("Question_input_id").value
-  cuisine_selected = document.getElementById("cuisine_id").value
-  recipe_selected = document.getElementById("recipe_id").value
-  servings = document.getElementById("Servings_id").value
-
-  if (speechToText){
-    question_asked = speechToText
+    return nodes;
   }
 
-  $.ajax({
-    type: "POST",
-    url: "/Query",
-    data: {
-      "question" : question_asked,
-      "direction_index" : direction_index,
-      "cuisine" : cuisine_selected, 
-      "recipe"  : recipe_selected, 
-      "servings": servings
-    },success: function (data, status, xhr) {
-      post_answer(data)
-    },
-    error: function (jqXhr, textStatus, errorMessage) {
-      console.log(errorMessage);
-    }
-  })
-
+  const root = [{ id: idCounter++, title: jsonObj.Title, subs: processSubTopics(jsonObj["Sub Topics"]) }];
+  return root;
 }
 
-function post_answer(answer){
-
-  document.getElementById('Answer_div').innerHTML = ""
-
-    var para = document.createElement("p");
-    var node_1 = document.createTextNode(answer);
-    para.appendChild(node_1);
-    var element = document.getElementById("Answer_div");
-    element.appendChild(para);
-
-  speakTheAnswer(answer)
-}
-
-function speakTheAnswer(answer){
-
-  var msg = new SpeechSynthesisUtterance(answer);
-  window.speechSynthesis.speak(msg);
-
-}
-
-function runSpeechRecognition() {
-  // get output div reference
-  var output = document.getElementById("output");
-  // get action element reference
-  var action = document.getElementById("action");
-      // new speech recognition object
-      var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
-      var recognition = new SpeechRecognition();
+function initializeComboTree(data) {
+  // Clear existing data in the comboTree, if any
+  console.log("Populating with Data", data)
   
-      // This runs when the speech recognition service starts
-      recognition.onstart = function() {
-          action.innerHTML = "<small>listening, please speak...</small>";
-      };
-      
-      recognition.onspeechend = function() {
-          action.innerHTML = "<small>stopped listening, hope you are done...</small>";
-          recognition.stop();
-      }
-    
-      // This runs when the speech recognition service returns result
-      recognition.onresult = function(event) {
-          var transcript = event.results[0][0].transcript;
-          var confidence = event.results[0][0].confidence;
-          //output.innerHTML = "<b>Text:</b> " + transcript + "<br/> <b>Confidence:</b> " + confidence*100+"%";
-          output.innerHTML = "<b>You Said:</b> " + transcript + "<br/> <b>";
-          output.classList.remove("hide");
-          console.log("confidence", confidence)
-          ask_question(transcript)
-      };
-    
-       // start recognition
-       recognition.start();
+  $('#justAnInputBox1').empty();
 
+  // Initialize the comboTree with the provided data
+  $('#justAnInputBox1').comboTree({
+    source: data,
+    isMultiple: true // Set to true if you want to allow multiple selections
+    // Other options and settings for the comboTree can be specified here
+  });
 }
-
--- Source_code/Flask Application/app.py
+++ Source_code/Flask Application/app.py
@@ -1,43 +1,24 @@
 from flask import Flask, render_template, request
 import json
-#from Web_Scraping import Web_Scrapper
-#from recipes_mapping import Recipe_Mapping
-#from Qurey_Resolver import Query_Resolver
+import os
+import sys
+from dotenv import load_dotenv
+
+load_dotenv(os.sep.join([os.path.dirname(os.getcwd()), "Scraping", ".env"])) 
+
+sys.path.insert(0, os.sep.join([os.path.dirname(os.getcwd()), os.getenv("SCRAPING_SCRIPT")]))
+from file_path_manager import File_Path_Manager
+
 
 app = Flask(__name__)
+fpm_h = File_Path_Manager()
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
 @app.route('/create_file', methods=['POST'])
-def create_file():
-    if request.method == 'POST':
-        '''
-        rm_h = Recipe_Mapping()
-        ws_h = Web_Scrapper()
 
-        cuisine = request.form.get('cuisine')
-        recipe = request.form.get('recipe')
-
-        recipe_link_file = "data/recipe_links.json"
-        recipe_link_data = json.load(open(recipe_link_file))
-
-        ws_h.recipe_handle.cuisine_names  = {cuisine : ""}
-        ws_h.recipe_handle.recipe_dict = {cuisine : { recipe : recipe_link_data[cuisine][recipe]} }
-        
-        print("calling the script", ws_h.recipe_handle.recipe_dict )
-
-        ws_h.scrape_recipes()
-        print("calling the script")
-
-        recipe_json = "data/recipe_details_1.json"
-        recipe_data = json.load(open(recipe_json))
-
-        print("Cuisine :{}, recipe {}".format(cuisine, recipe))
-
-        return (recipe_data[cuisine][recipe])
-        '''
 
 @app.route('/Query', methods=['POST'])
 def Query():
@@ -62,24 +43,11 @@ def Query():
 
 @app.route('/data')
 def get_data():  
-    json_path = "data/recipe_links.json"
+
+    json_path = os.sep.join([fpm_h.get_book_json_dir(), "Machine_Learning_A_Probabilistic_Perspective_Index.json"])
     data = json.load(open(json_path)) 
-    ##print(data)
+    print(data)
     return json.dumps(data)
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
-
-'''
-a. User can read the comments (or listen)
-b. Navigate between steps - Done in Front end.
-b.1 Go forward - Done with Next Direction.
-b.2 Go backward - Done with Prev Direction. 
-c. ask questions about 
-  c.1. cooking action specified in the step - 
-c.2. the ingredients used - 
-c.3 the utensil or tool used
-c.4 time and temp
-'''
